940710be9c14b991d6e63b305b28d8247d140b83
Min Thu Aung <reoknite@gmail.com>
2019-06-15 20:47:15 -0400

    Updates

---
+++ precache-manifest.b38da625659955a3f099b2e4955c3624.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1fff9c729f5657fa807e83360dfffc68",
+    "url": "/calculator/index.html"
+  },
+  {
+    "revision": "0acc37978d23c4f4e0f1",
+    "url": "/calculator/static/css/main.ab757798.chunk.css"
+  },
+  {
+    "revision": "9eee67cf782a38d35aeb",
+    "url": "/calculator/static/js/2.d6a534e5.chunk.js"
+  },
+  {
+    "revision": "0acc37978d23c4f4e0f1",
+    "url": "/calculator/static/js/main.59b353d5.chunk.js"
+  },
+  {
+    "revision": "2fe8cbdbbea1fbf116e6",
+    "url": "/calculator/static/js/runtime~main.2539f434.js"
+  }
+]);
